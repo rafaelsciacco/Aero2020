88cd08b6eb3503833d2731bb1ec56efbc27abcd7
rafaelsciacco <rafaelsciacco@gmail.com>
2020-01-08 15:21:00 -0300

    rpm funcional

---
+++ Principal2020/Principal2020.ino
@@ -0,0 +1,34 @@
+//Variaveis rpm
+#define pinINT   27
+volatile int conta_RPM = 0;
+int copyconta_RPM = 0;
+float RPM = 0;
+float contaAtualMillis = 0;
+float antigoMillis = 0;
+float subMillis = 0;
+float minutos = 0;
+
+//Funcao interrupcao
+void IRAM_ATTR ContaInterrupt() {
+  conta_RPM = conta_RPM + 1; //incrementa o contador de interrupções
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pinINT,INPUT_PULLUP);
+  attachInterrupt(pinINT,ContaInterrupt, RISING);
+
+  delayMicroseconds(1000);
+}
+
+void loop() {
+  copyconta_RPM = conta_RPM;
+  contaAtualMillis = millis(); 
+  subMillis = contaAtualMillis - antigoMillis;
+  minutos = (subMillis/(60000));
+  RPM = copyconta_RPM/(4*minutos);
+  antigoMillis = contaAtualMillis;
+  conta_RPM = 0;
+  Serial.println(RPM);
+  delayMicroseconds(1000000);
+}
