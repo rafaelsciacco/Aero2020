089f03a3315eb402ea90dbefc46e8b395f41897f
rafaelsciacco <rafaelsciacco@gmail.com>
2020-01-08 15:36:40 -0300

    programacao tempo adicionada
    rpm e tempo funcionais

--- Principal2020/Principal2020.ino
+++ Principal2020/Principal2020.ino
@@ -1,3 +1,6 @@
+//Bibliotecas
+#include <RTClib.h>
+
 //Variaveis rpm
 #define pinINT   27
 volatile int conta_RPM = 0;
@@ -8,6 +11,10 @@ float antigoMillis = 0;
 float subMillis = 0;
 float minutos = 0;
 
+//Variaveis tempo
+float tempo  = 0;
+RTC_DS1307 rtc;
+
 //Funcao interrupcao
 void IRAM_ATTR ContaInterrupt() {
   conta_RPM = conta_RPM + 1; //incrementa o contador de interrupções
@@ -17,7 +24,17 @@ void setup() {
   Serial.begin(115200);
   pinMode(pinINT,INPUT_PULLUP);
   attachInterrupt(pinINT,ContaInterrupt, RISING);
-
+  
+  if (! rtc.begin()) {
+    Serial.println("RTC nao detectado");
+    while (1);
+  }
+  //rtc.adjust(DateTime(2019, 10, 26, 15, 22, 0));  // (Ano,mês,dia,hora,minuto,segundo)
+  
+  Serial.print("RPM");
+  Serial.print("  ");
+  Serial.print("Tempo");
+  Serial.println("  ");
   delayMicroseconds(1000);
 }
 
@@ -29,6 +46,12 @@ void loop() {
   RPM = copyconta_RPM/(4*minutos);
   antigoMillis = contaAtualMillis;
   conta_RPM = 0;
-  Serial.println(RPM);
+  Serial.print(RPM);
+  Serial.print("  ");
+
+  DateTime now = rtc.now();
+  tempo = ((now.hour()*3600)+(now.minute()*60)+(now.second()));
+  Serial.print(tempo);
+  Serial.println("  ");
   delayMicroseconds(1000000);
 }
